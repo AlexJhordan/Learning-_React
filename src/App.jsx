b775b371d406d7c90bbfbaa51f6726744d84e1fb
Alex <alex.ajct01@gmail.com>
2024-07-15 14:09:02 -0300

    Projeto criado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit accusantium quibusdam inventore sit nesciunt corrupti assumenda est odit itaque ipsa? Nulla aliquid fugiat sapiente at! Excepturi expedita quis nihil perferendis.</p>
+    </div>
+  );
+}
+
+export default App;
